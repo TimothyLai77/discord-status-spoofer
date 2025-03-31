3e9fe60f5439a4d35b69beab0cacfa8ac366e67b
timothy <lai.timothy77@gmail.com>
2025-03-31 16:32:22 -0400

    async runner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import discord # specifically use the discord.py-self fork, since idk seems to work better?
 import os
 from dotenv import load_dotenv
-import discord # specifically use the discord.py-self fork, since idk seems to work better?
+import asyncio
 
 # get discord user token from .env
 load_dotenv()
@@ -9,9 +10,25 @@ token = os.getenv('TOKEN')
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
-        await client.change_presence(status=discord.Status.dnd, afk=True)
+        print(f'Logged in as {self.user}')
+        # await client.change_presence(status=discord.Status.dnd, afk=True)
+        await changeStaus(discord.Status.dnd,True)
 
-client = MyClient()
-client.run(token)
+
+async def changeStaus(status, isAFK=True):
+    await client.change_presence(status=status, afk=True)
+
+
+async def login():
+    client = MyClient()
+    await client.login(token)
+    await clinet.connect(reconnect=True)
+
+def main():
+    print('Starting..')
+    asyncio.run(login())
+
+
+
+if __name__ == "__main__":
+    main()
