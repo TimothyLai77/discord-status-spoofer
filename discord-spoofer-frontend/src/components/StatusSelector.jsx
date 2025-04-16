0a9fcd41677c6c97f7c96033fb60ce748669ac8b
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-04-16 14:27:09 -0400

    added axios, and progress

--- discord-spoofer-frontend/src/components/StatusSelector.jsx
+++ discord-spoofer-frontend/src/components/StatusSelector.jsx
@@ -3,12 +3,25 @@ import { Card } from "@chakra-ui/react"
 import { Button } from "@chakra-ui/react"
 import { Switch } from "@chakra-ui/react"
 import { useEffect, useState } from "react";
+import axios from "axios"
+
 const StatusSelector = () => {
+
+    const handleButtonClick = async (status) => {
+        try{
+            const json = {newStatus: status, isAfk: true}
+            const reponse = await axios.post('http://127.0.0.1:8081/updateStatus', json)
+        }catch (error){
+            console.error(`Error: ${error}`)
+        }
+    }
+
     const [status, setStatus] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try{
+                // todo: chnage this to use axios
                 const response = await fetch('http://127.0.0.1:8081/getStatus')
                 const result = await response.json()
                 const currentStatus = result.currentStatus
@@ -27,10 +40,10 @@ const StatusSelector = () => {
             Current status: {status}
         </Card.Header>
         <Card.Body gap="2">
-            <Button>online</Button>
-            <Button>away</Button>
-            <Button>do not disturb</Button>
-            <Button>invisible</Button>
+            <Button onClick={() => handleButtonClick("online")}>online</Button>
+            <Button onClick={() => handleButtonClick("away")}>away</Button>
+            <Button onClick={() => handleButtonClick("dnd")}>do not disturb</Button>
+            <Button onClick={() => handleButtonClick("invisible")}>invisible</Button>
         </Card.Body>
         <Card.Footer justifyContent={"left"}>
         <Switch.Root>
