30413b89ca53129be21f1e8964103d6834c6820f
timothy <lai.timothy77@gmail.com>
2025-04-15 22:48:51 -0400

    template

--- discord-spoofer-frontend/src/components/StatusSelector.jsx
+++ discord-spoofer-frontend/src/components/StatusSelector.jsx
@@ -1,18 +1,28 @@
 
 import { Card } from "@chakra-ui/react"
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
+import { Switch } from "@chakra-ui/react"
 const StatusSelector = () => {
   return (
     <Card.Root>
-        <Card.Header />
-            <Card.Body>
-                <Button>online</Button>
-                <Button>away</Button>
-                <Button>do not disturb</Button>
-                <Button>invisible</Button>
-
-            </Card.Body>
-        <Card.Footer />
+        <Card.Header>
+            Current status: 
+        </Card.Header>
+        <Card.Body gap="2">
+            <Button>online</Button>
+            <Button>away</Button>
+            <Button>do not disturb</Button>
+            <Button>invisible</Button>
+        </Card.Body>
+        <Card.Footer justifyContent={"left"}>
+        <Switch.Root>
+        <Switch.HiddenInput />
+            <Switch.Control>
+                <Switch.Thumb />
+            </Switch.Control>
+            <Switch.Label> AFK Enabled (enable push notifications) </Switch.Label>
+        </Switch.Root>
+        </Card.Footer>
     </Card.Root>
   );
 };
