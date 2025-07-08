3a11ae1b5637efc0292c2d452e685907dfde6a65
Timothy Lai <33475176+TimothyLai77@users.noreply.github.com>
2025-07-08 00:48:01 -0400

    resume from internet disconnect and documentation

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const path = require("path");
 const CLIENT_FRONTEND_PATH = path.join(__dirname, "./", "discord-spoofer-frontend", "dist");
 const PORT = process.env.PORT;
 
-const { Client } = require('discord.js-selfbot-v13');
+
 const app = express();
 app.use(express.json());
+
+const { Client } = require('discord.js-selfbot-v13');
+const TOKEN = process.env.TOKEN;
+const client = new Client();
+
+var statusAlreadySet = false;
+var lastStatus = null;
+var lastAfk = null;
 
 const prepareApp = async () => {
     app.set("trust proxy", 1);
@@ -30,12 +38,12 @@ const prepareApp = async () => {
 // GET current status that the account has at the moment
 app.get('/api/getStatus', async (req, res) => {
     console.log('GET: /api/status')
-    try{
+    try {
         const presence = client.user.presence;
         res.json({
             currentStatus: presence.status
         })
-    }catch{
+    } catch {
         res.status(500);
         res.send('error getting user status')
     }
@@ -44,26 +52,28 @@ app.get('/api/getStatus', async (req, res) => {
 
 // update the status {newStatus: "status", isAfk: bool} 
 // isAfk is a leftover from the python version, i don't think discord.js supports this?
-app.post('/api/updateStatus', async (req,res)=>{
-    try{
+app.post('/api/updateStatus', async (req, res) => {
+    try {
         // {newStatus: status, isAfk: true}
         console.log("POST: /api/updateStatus")
         const payload = req.body;
         const status = payload.newStatus;
         // TODO: set the status here to resume from on internet outage
         await changeStatus(status);
-        res.send(`changed status to ${status}`,200);
-    }catch{
+        res.send(`changed status to ${status}`, 200);
+    } catch {
         res.status(500);
         res.send('error')
     }
 });
 
 
-
-const TOKEN = process.env.TOKEN;
-const client = new Client();
-const changeStatus = async (newStatus, isAfk=true) => {
+/**
+ * change status 
+ * @param {string} newStatus 'online', 'idle', 'dnd', 'invisible'
+ * @param {boolean} isAfk afk to decide if Discord should send a push notification to mobile
+ */
+const changeStatus = async (newStatus, isAfk = true) => {
     // 'online', 'idle', 'dnd', 'invisible'
     console.log(`Changing status to: ${newStatus}`)
     client.user.setPresence(
@@ -72,15 +82,26 @@ const changeStatus = async (newStatus, isAfk=true) => {
             afk: isAfk
         }
     ) // https://discordjs-self-v13.netlify.app/#/docs/docs/main/typedef/PresenceData
-    
-    //client.user.setStatus(newStatus);
-
+    statusAlreadySet = true;
+    lastStatus = newStatus;
+    lastAfk = isAfk;
 }
 
 client.on('ready', async () => {
     console.log(`${client.user.username} is ready!`);
-    changeStatus('invisible', true);
+    changeStatus('online', true); // just default status as online
+
+
 })
+// idk which one to use https://gist.github.com/Iliannnn/6c69605cb6b8cc03f0ab9c885fd39906#apirequest
+client.on('shardReady', (id) => {
+    if (statusAlreadySet) {
+        console.log(`resuming ${lastStatus}`)
+        changeStatus(lastStatus, lastAfk)
+    } else {
+        changeStatus('invisible', true); // default as invisible when resuming
+    }
+});
 
 client.login(TOKEN);
 
